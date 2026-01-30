7c54e7294297d2e662260f2be4684ac98a1f84f4
jungwj09 <woox1jin@gmail.com>
2026-01-30 18:56:55 +0900

    fix: resolve build error

--- src/utils/meeting-room.ts
+++ src/utils/meeting-room.ts
@@ -7,8 +7,11 @@ export const buildMeetingRoomRequestIntoSchedule = (data: ListResponse<MeetingRo
   return data.items.map((item) => ({
     id: item.id,
     title: item.borrower.name,
+    description: item.purpose || "",
     startDate: item.wantedDate,
     endDate: item.wantedDate,
-    color: "red"
+    color: "#FF8E94",
+    type: "ACADEMIC" as const,
+    eventId: null,
   }))
 }
