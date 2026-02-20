55af12d3710187fc729e773a4aee2a4cf1efe016
jungwj09 <woox1jin@gmail.com>
2026-02-20 15:35:24 +0900

    style: fix incorrect styles

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="w-full px-[10px]" style={{ marginTop: "64px" }}>
+    <div className="w-full px-[10px] py-2">
       <div className="relative w-full h-[1080px] rounded-[24px] overflow-hidden">
         <Image
           src="/hero_background.svg"
